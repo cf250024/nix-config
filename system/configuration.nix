79553fd54d5e14dee0dbccd022ed17524d056cdb
Chengjun Fu <cfu.usa@gmail.com>
2024-03-12 21:21:19 -0700

    check-in users files

--- system/configuration.nix
+++ system/configuration.nix
@@ -146,7 +146,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-  #virtualisation.docker.enable = true;
+  virtualisation.docker.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   programs.nix-ld.enable = true;
