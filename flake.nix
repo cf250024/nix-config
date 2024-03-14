33edb9a126c83ab93de020937058970ecdcd7574
Chengjun Fu <cfu.usa@gmail.com>
2024-03-13 21:00:02 -0700

    update

--- flake.nix
+++ flake.nix
@@ -9,33 +9,31 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: 
+  outputs = { nixpkgs, blender-bin, home-manager, ... }: 
   let
     system = "aarch64-linux";
+    username = "nixos";
     pkgs = import nixpkgs {
       inherit system;
       config = { allowUnfree = true; };
     };
     lib = nixpkgs.lib;
   in {
-    homeManagerConfigurations = {
-      nixos = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs;
-        homeManagerConfiguration = {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            ./home.nix
-            {
-              home = {
-                username = "nixos";
-                homeDirectory = "/home/nixos";
-                stateVersion = "22.05";
-              };
-            }
-          ];
-        };
+    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      configuration = {
+        imports = [
+          ./home.nix
+        ];
+        nixpkgs.overlays = [blender-bin.overlays.default];
       };
+
+      inherit system username;
+      homeDirectory = "/home/${username}";
+      # Update the state version as needed.
+      stateVersion = "22.05";
     };
+
 
     nixosConfigurations = {
       nixos = lib.nixosSystem {
